db229f4781b193425893d1d107713bc62a571ad7
AdithiKashain <94888785+AdithiKashain@users.noreply.github.com>
2024-02-02 22:46:30 +0530

    Update locators.py

--- locators.py
+++ locators.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 
+import files
+
 
 new_button_selector = (By.CSS_SELECTOR,"button.brbsPe.Ss7qXc.a-qb-d")
 file_upload_button_selector = (By.CSS_SELECTOR,"div[data-tooltip='File upload'].a-v-T")
@@ -8,3 +10,16 @@ download_file_selector = (By.CSS_SELECTOR,"div[aria-label='Download'].pc7nUb.kXQ
 upload_complete_text = (By.CSS_SELECTOR,"span[aria-label='1 upload complete'].af-V")
 file_already_present_text = (By.CSS_SELECTOR,"div#EJL26d")
 file_name_containerdiv = (By.CSS_SELECTOR,"div.KL4NAf")
+
+
+
+ok_button_locator = (By.XPATH,'//button[@name="ok" and contains(@class, "h-De-Vb h-De-Y")]',)
+file_name_selector = (By.XPATH, f'//div[@class="uXB7xe" and contains(@aria-label,"{files.file_name}" )]',)
+renamed_file_name_selector = (By.XPATH, f'//div[@class="uXB7xe" and contains(@aria-label,"{files.renamed_file_name}" )]',)
+
+
+trash_button_locator = (By.XPATH, '//div[@aria-label="Trash"]')
+trashed_file_locator = (By.XPATH, f'//div[@class="uXB7xe" and contains(@aria-label,"{files.trashed_file_name}" )]',)
+home_button_locator = (By.XPATH, '//div[@aria-label="Home"]')
+restored_file_locator = (By.XPATH, f'//div[@class="uXB7xe" and contains(@aria-label,"{files.trashed_file_name}" )]',)
+restore_from_trash_button_locator = (By.XPATH, '//div[@aria-label="Restore from trash"]')
