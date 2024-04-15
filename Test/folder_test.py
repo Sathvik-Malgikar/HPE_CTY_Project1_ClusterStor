28b21848ab75f6bd69c2ad03492652fa0d031e93
sid692 <119108353+sid692@users.noreply.github.com>
2024-04-15 18:15:30 +0530

    added flake8

--- Test/folder_test.py
+++ Test/folder_test.py
@@ -1,14 +1,11 @@
 import sys
 import inspect
-
-sys.path.append(r'C:\HPE_CTY_Project1_ClusterStor')
-
-
 from infrastructure import locators
 import files
 from infrastructure import autoGUIutils
+from base_class import Base, toast_testcase_name, plain_toast
+sys.path.append(r'C:\HPE_CTY_Project1_ClusterStor')
 
-from base_class import Base,toast_testcase_name,plain_toast
 
 class TestfolderActions(Base):
 
@@ -17,30 +14,24 @@ class TestfolderActions(Base):
     """
     @classmethod
     def setup_class(cls):
-        super(cls, TestfolderActions).setup_class()#FIRST SUPER CLASS
+        super(cls, TestfolderActions).setup_class()  # FIRST SUPER CLASS
         plain_toast("Executing suite : " + cls.__name__, f"Contains {len(inspect.getmembers(TestfolderActions,inspect.isfunction))} testcases")
-
-        #THEN SUBCLASS SETUP
-        folders_to_create = [files.renamed_folder_name , files.destination_folder_name, files.create_folder_name,files.folder_to_be_moved,files.folder_name, files.folder_name_to_be_removed]
-        
+        # THEN SUBCLASS SETUP
+        folders_to_create = [files.renamed_folder_name, files.destination_folder_name, files.create_folder_name, files.folder_to_be_moved, files.folder_name, files.folder_name_to_be_removed]
         for folder_name in folders_to_create:
             cls.higher_actions.click_on_new_button()
             action_button = cls.higher_actions.wait_to_click(locators.new_menu_button_locator("New folder"))
             action_button.click()
-            
             autoGUIutils.type_into_dialogue_box(folder_name)
             cls.higher_actions.refresh_and_wait_to_settle()
 
-       
-    
     @classmethod
     def teardown_class(cls):
-        # #FIRST SUBCLASS TEARDOWN LOGIC
+        # FIRST SUBCLASS TEARDOWN LOGIC
         # folders_to_clean = [files.renamed_folder_name , files.destination_folder_name,files.create_folder_name]
         # for foldername in folders_to_clean:
         #     cls.higher_actions.remove_file_action(foldername) # remove_file_action works for both file and folder
-        
-        super(cls, TestfolderActions).teardown_class()#THEN SUPERCLASS TEARDOWN
+        super(cls, TestfolderActions).teardown_class()  # THEN SUPERCLASS TEARDOWN
 
     @toast_testcase_name
     def test_rename_folder(self):
@@ -52,13 +43,12 @@ class TestfolderActions(Base):
     """
     ## Test function to create a new folder in the Google Drive web GUI.
     """
-    
+
     @toast_testcase_name
     def test_create_folder(self):
         folder_name = files.create_folder_name
         self.higher_actions.create_folder_action(folder_name)
         assert self.higher_actions.wait_for_element(locators.file_selector(folder_name)) is not None
-        
 
     """
     ## Test function to upload new file in the Google Drive web GUI.
@@ -77,7 +67,7 @@ class TestfolderActions(Base):
         # self.higher_actions.navigate_to("Home")
         # self.higher_actions.click_on_folders_button()
         self.higher_actions.move_action(foldername, destination_folder)
-        self.higher_actions.verify_file_in_destination(foldername,destination_folder)
+        self.higher_actions.verify_file_in_destination(foldername, destination_folder)
         self.higher_actions.navigate_to("My Drive")
         self.driver.refresh()
         assert not self.higher_actions.wait_for_element(locators.file_selector(foldername))
